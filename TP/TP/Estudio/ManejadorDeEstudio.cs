e59d5a5195ac8c0fa36ba8fa7d0c86de547f5c72
Martin-Paez <104704565+Martin-Paez@users.noreply.github.com>
2022-05-25 14:07:17 -0300

    Add files via upload

---
+++ TP/TP/Estudio/ManejadorDeEstudio.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 23/05/2022
+ * Time: 19:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace EstudioNS
+{
+	/// <summary>
+	/// Description of Manejador.
+	/// </summary>
+	public class ManejadorDeEstudio
+	{
+		public ManejadorDeEstudio()
+		{
+		}
+		
+		public static void resolver(string err, Estudio e){
+			switch(err) {
+				case "E1":
+					dniAbogado(e,"Hay un abogado registrado con el mismo DNI");
+					break;
+				case "E2":
+					numeroExpediente(e,"Hay un expediente registrado con el mismo numero");
+					break;
+				case "A1":
+					expedientesAsignados(e,"El abogado ya tiene demasiados expedientes asignados");
+					break;
+				default:
+					errInesperado("Surgio un error inesperado");
+					break;
+			}
+			
+		}
+		
+		protected static int dniAbogado(Estudio e, string err){
+			Console.WriteLine(err);
+		}
+		
+		protected static int numeroExpediente(Estudio e, string err){
+			Console.WriteLine(err);
+		}
+			
+		protected static int expedientesAsignados(Estudio e, string err){
+			Console.WriteLine(err);
+		}
+		
+		protected static void errInesperado(string err){
+			Console.WriteLine(err);
+		}
+					
+	}
+}
--- TP/TP/Program.cs
+++ TP/TP/Program.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using EstudioNS;
 
 namespace TP
 {
@@ -16,18 +17,10 @@ namespace TP
 		public static void Main(string[] args)
 		{
 			Estudio e = initWorld();
-			string item;
-			do {
-				item="0";
-				imprimirMenu();
-				Console.Write("> Numero de Opcion: ");
-				item = Console.ReadLine();
-				resolverItem(item, e);
-			} while( item != "9" );
-		
+			while( resolverItem( elegirItem() , e) );
 		}
 		
-		private static void resolverItem(string item, Estudio e){
+		private static bool resolverItem(string item, Estudio e){
 			Console.Clear();
 			string exit = "\nPresiona una tecla para volver al menu. . . ";
 			switch(item.Trim()){
@@ -52,8 +45,10 @@ namespace TP
 					break;
 				case "8":
 					break;
-				case"9":
-					exit = "Programa terminado con exito";
+				case "9":
+					return false;
+				case "devMode":
+					exit = "Modo desarrollador deshabilitado";
 					break;
 				default:
 					exit = "Opcion invalida";
@@ -61,48 +56,42 @@ namespace TP
 			}
 			Console.Write(exit);
 			Console.ReadKey(true);
+			return true;
 		}
 		
-		public static void AgregarExpediente(Estudio estudio) {
-			Console.WriteLine("Opcion: AGREGAR EXPEDIENTE\n");
-			Console.Write("Numero: ");
-			string numero = Console.ReadLine();
-			Console.Write("Titular: \n");
-			Persona p = crearPersona();
-			Console.Write("Dni del Abogado: ");
-			Abogado a = estudio.GetAbogado(Console.ReadLine());
-			Console.Write("Tipo: ");
-			string tipo = Console.ReadLine();
-			Console.Write("Estado: ");
-			string estado = Console.ReadLine();
-			Expediente e = new Expediente(numero, p, tipo, estado, a, DateTime.Today);
-			try {
-				estudio.AgregarExpediente( e );
-			} catch (Exception err) {
-				Manejador.resolver(err.Message);
-			}
+		public static string elegirItem(){
+			Console.Clear();
+			Console.WriteLine("1) Agregar abogado");
+			Console.WriteLine("2) Eliminar abogado");
+			Console.WriteLine("3) Listado de abogados");
+			Console.WriteLine("4) Listado de expedientes");
+			Console.WriteLine("5) Agregar expediente");
+			Console.WriteLine("6) Modificar el estado de un expediente");
+			Console.WriteLine("7) Eliminar expediente por numero ");
+			Console.WriteLine("8) Listado de expedientes de tipo ‘audiencia'");
+			Console.WriteLine("9) Salir \n");
+			Console.Write("> Numero de Opcion: ");
+			return Console.ReadLine();
 		}
-		
+
 		public static void AgregarAbogado(Estudio e){
 			Console.WriteLine("Opcion: AGREGAR ABOGADO \n");
-			Persona p = crearPersona();
-			Console.Write("Especializacion: ");
-			string espec = Console.ReadLine();
+			string[] d = LeerDatos("Nombre/Apellido/DNI");
 			try {
-				e.AgregarAbogado( new Abogado(p.Nombre, p.Apellido, p.Dni, espec));
+				Console.Write("Especializacion: ");
+				e.AgregarAbogado( new Abogado(d[0],d[1],d[2], Console.ReadLine()) );
 			} catch (Exception err) {
-				Manejador.resolver(err.Message);
+				ManejadorDeEstudio.resolver(err.Message);
 			}
 		}
 		
-		public static Persona crearPersona(){
-			Console.Write("Nombre: ");
-			string nombre = Console.ReadLine();
-			Console.Write("Apellido: ");
-			string apellido = Console.ReadLine();
-			Console.Write("DNI: ");
-			string dni = Console.ReadLine();
-			return new Persona(nombre, apellido, dni);
+		public static string[] LeerDatos(string nombres){
+			string[] split = nombres.Split('/');
+			for(int i=0; i<split.Length; i++) {
+				Console.Write("  "+split[i]+": ");
+				split[i] = Console.ReadLine();
+			}
+			return split;
 		}
 	
 		public static void EliminarAbogado(Estudio e){
@@ -114,51 +103,49 @@ namespace TP
 				Console.WriteLine("No encontrado");
 		}
 		
-		public static void ImprimirAbogados(Estudio e) {
-			Console.WriteLine("Opcion: IMPRIMIR ABOGADOS \n");
-			if ( e.Abogados.Count == 0 )
-				Console.WriteLine("No hay abogados");
-			else
-				foreach(Abogado a in e.Abogados) {
-				ImprimirAbogado(a);
-				}
-		}
-		
-		public static void ImprimirAbogado(Abogado a) {
-			ImprimirPersona(a);
-			Console.WriteLine("Especializacion: " + a.Espec + "\n");
-		}
+		public static void AgregarExpediente(Estudio estudio) {
+			Console.WriteLine("Opcion: AGREGAR EXPEDIENTE\n");
+			string[] d = LeerDatos("Tipo/Estado/Nombre del titular/Apellido del titular/DNI del titular");
+			Persona p = new Persona(d[2],d[3],d[4]);
 			
-		public static void ImprimirExpedientes(Estudio estudio) {
-			Console.WriteLine("Opcion: IMPRIMIR EXPEDIENTES\n");
-			if ( estudio.Expedientes.Count == 0 )
-				Console.WriteLine("No hay expedientes");
-			else
-				foreach(Expediente e in estudio.Expedientes) {
-					Console.WriteLine("Numero de expediente: " + e.Numero);
-					Console.WriteLine("Estado: " + e.Estado);
-					Console.WriteLine("Tipo: " + e.Tipo);
-					Console.WriteLine("Fecha de creacion: " + e.FechaCreacion);
-					Console.WriteLine("\nDatos del titular: ");
-					ImprimirPersona(e.Titular);
-					if (e.Abogado != null) {
-						Console.WriteLine("\nDatos del abogado: ");
-						ImprimirAbogado(e.Abogado);
-					} else 
-						Console.WriteLine("\nNo tiene un abogado asignado");
+			string rta;
+			Abogado a;
+			do {
+				rta="";
+				Console.Write("\nDni del Abogado: ");
+				a = estudio.GetAbogado(Console.ReadLine());
+				if ( a == null )
+					Console.WriteLine("No se encontro abogado");
+				else if(a.CantExpedientes>=6)
+					Console.WriteLine("El abogado tiene demasiados expedientes asignados");
+				else
+					rta="S"; //Cargado con Exito
+				while ( rta != "S" & rta != "N" ) {
+					Console.WriteLine("¿Desea dejar el expediente sin asignar? S/N");
+					rta = Console.ReadLine().ToUpper();
+				} 					
+			} while ( rta != "N");
+			
+			bool ok;
+			do {
+				ok = false;
+				Console.Write("Numero: ");
+				Expediente e = new Expediente(Console.ReadLine(),p,d[0],d[1],a,DateTime.Today); // Lo creamos aca porque pidio la Profe
+				try {
+					estudio.AgregarExpediente( e );
+				} catch (Exception err) {
+					ManejadorDeEstudio.resolver(err.Message);
 					Console.WriteLine("");
+					ok=true;
 				}
-		}
-		
-		private static void ImprimirPersona(Persona p) {
-			Console.WriteLine("Nombre y apellido: " + p.Nombre + " " + p.Apellido);
-			Console.WriteLine("DNI: " + p.Dni);
+			} while(ok);
 		}
 		
 		private static void modifExpediente(string numero){
 			
 		}
 		
+/*------------------------- CARGAR DATOS / ARCHIVOS -----------------------------------*/
 		
 		public static Estudio initWorld(){
 			Estudio estudio = new Estudio();
@@ -174,18 +161,48 @@ namespace TP
 			return estudio;
 		}
 		
-		public static void imprimirMenu(){
-			Console.Clear();
-			Console.WriteLine("1) Agregar abogado");
-			Console.WriteLine("2) Eliminar abogado");
-			Console.WriteLine("3) Listado de abogados");
-			Console.WriteLine("4) Listado de expedientes");
-			Console.WriteLine("5) Agregar expediente");
-			Console.WriteLine("6) Modificar el estado de un expediente");
-			Console.WriteLine("7) Eliminar expediente por numero ");
-			Console.WriteLine("8) Listado de expedientes de tipo ‘audiencia'");
-			Console.WriteLine("9) Salir \n");
+/*-------------------------IMPRIMIR POR PANTALLA ---------------------------------------*/
+		
+		private static void ImprimirPersona(Persona p) {
+			Console.WriteLine("Nombre y apellido: " + p.Nombre + " " + p.Apellido);
+			Console.WriteLine("DNI: " + p.Dni);
 		}
-	
+		
+		public static void ImprimirAbogado(Abogado a) {
+			ImprimirPersona(a);
+			Console.WriteLine("Especializacion: " + a.Espec + "\n");
+		}
+		
+		public static void ImprimirAbogados(Estudio e) {
+			Console.WriteLine("Opcion: IMPRIMIR ABOGADOS \n");
+			if ( e.Abogados.Count == 0 )
+				Console.WriteLine("No hay abogados");
+			else
+				foreach(Abogado a in e.Abogados) {
+				ImprimirAbogado(a);
+				}
+		}
+				
+		public static void ImprimirExpedientes(Estudio estudio) {
+			Console.WriteLine("Opcion: IMPRIMIR EXPEDIENTES\n");
+			if ( estudio.Expedientes.Count == 0 )
+				Console.WriteLine("No hay expedientes");
+			else
+				foreach(Expediente e in estudio.Expedientes) {
+					Console.WriteLine("Numero de expediente: " + e.Numero);
+					Console.WriteLine("Estado: " + e.Estado);
+					Console.WriteLine("Tipo: " + e.Tipo);
+					Console.WriteLine("Fecha de creacion: " + e.FechaCreacion);
+					Console.WriteLine("\nDatos del titular: ");
+					ImprimirPersona(e.Titular);
+					if (e.Abogado != null) {
+						Console.WriteLine("\nDatos del abogado: ");
+						ImprimirAbogado(e.Abogado);
+						Console.WriteLine("");
+					} else 
+						Console.WriteLine("\nNo tiene un abogado asignado \n");
+				}
+		}
+			
 	}
 }
