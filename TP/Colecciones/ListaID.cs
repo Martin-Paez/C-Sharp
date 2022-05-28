c219e520b9577649c4d1ead26fd3e06f4d332a5b
Martin-Paez <mpaez.ok@gmail.com>
2022-05-27 21:53:17 -0300

    Update ListaID.cs

--- TP/Colecciones/ListaID.cs
+++ TP/Colecciones/ListaID.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections;
-using EstudioNS;
+using ListaIdNS;
 
 namespace ListaIdNS
 {
     public abstract class ListaId {
-        protected ArrayList lista = new ArrayList();
+        private ArrayList lista = new ArrayList();
 
-        public int posicion(string id){
+        private int posicion(string id){
             int i = -1;
             while ( (++i<this.lista.Count) && ((Identificable)this.lista[i]).Id != id );
             if ( i >= this.lista.Count)
@@ -15,34 +15,67 @@ namespace ListaIdNS
             return i;
         }
 
-        public bool existe(string id){
+        private bool existe(string id){
             return posicion(id) > -1;
         }
 
-        public Identificable Get(string id) {
+        protected Identificable Get(string id) {
             int i = posicion(id);
             if ( i == -1 )
                 throw new DatoInvalido();
             return (Identificable) this.lista[i];
         }
-        
-        // IndexOutOfRangeException
-        public Identificable Get(int i){
-        	return (Identificable) this.lista[i];
+
+        // Excepcion IndexOutOfRangeException
+        public Identificable Get(int i) {
+            return this.list[i];
         }
 
         public void Agregar(Identificable e){
-			if ( this.existe(e.Id) )
+			if ( base.existe(a.Dni) )
 				throw new Repetido();
-			this.lista.Add(e);
+			this.list.Add(a);
 		}
 
         // Excepcion DatoInvalido()
         public Identificable Eliminar(string numero) {
 			Identificable e = this.Get(numero); // Excepcion DatoInvalido()
-			this.lista.Remove(e);
+			this.list.Remove(e);
             return e;
 		}
+
+    
+    }
+}
+
+/* NO conviene, hay que estar sobreescribiendo metodos de ingresos de datos, para evitar que cargen elementos no deseados
+
+
+using System;
+using System.Collections;
+using EstudioNS;
+
+namespace ListaIdNS
+{
+    public abstract class ListaId:ArrayList {
+
+        public int IndexOf(string id){
+            return base.IndexOf( new(Identificable(id)) );
+        }
+        public bool Contains(string id) {
+            return base.Contains( new(Identificable(id)) );
+        }
+
+        public Identificable Get(string id) {
+            int i = IndexOf(id);
+            if ( i == -1 )
+                throw new DatoInvalido();
+            return (Identificable) this[i];
+        }
+
+        public void Remove(string id){
+            base.Remove( new(Identificable(id)) );
+        }
 
         public override string ToString(){
         	string str = "";
@@ -51,9 +84,13 @@ namespace ListaIdNS
             }
         	return str;
         }
-        
-        public int Count{
-        	get {return this.lista.Count;}
+
+        public override int Add(object value)
+        {
+            throw new Exception("Operacion no permitida");
         }
+        
+
     }
 }
+*/
