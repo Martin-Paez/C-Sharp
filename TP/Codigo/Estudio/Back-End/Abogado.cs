be0d29b5d38054d588762e7b8d0c8341df351625
Martin-Paez <mpaez.ok@gmail.com>
2022-05-28 00:50:42 -0300

    Carpeta Codigo
    La carpeta guarda carpetas y archivos .cs
    
    Para separa del resto de cosas del proyecto

---
+++ TP/Codigo/Estudio/Back-End/Abogado.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 23/05/2022
+ * Time: 18:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace EstudioNS
+{
+	/// <summary>
+	/// Description of Abogado.
+	/// </summary>
+	public class Abogado : Persona
+	{
+		private string espec;
+		private int cantExps = 0;
+		private int maxExp = 6;
+		
+		public Abogado(string nombre, string apellido, string dni, string espec):base(nombre, apellido, dni)
+		{
+			this.espec = espec;
+		}
+		
+		public string Espec {
+			set{this.espec=value;}
+			get{return this.espec;}
+		}
+		
+		public int CantExps{
+			set{
+				if ( value>maxExp ) 
+					throw new DemasiadosExpedientes();
+				else if ( value<0 )
+					throw new FaltanExpedientes();
+				this.cantExps=value;
+			}
+			get{return this.cantExps;}
+		}
+
+		public int MaxExp{
+			get{return this.maxExp;}
+		}
+		
+		public override string ToString() {
+			return base.ToString() + "\nEspecializacion: " + this.espec + "\n";
+		}
+	}
+	
+	public class DemasiadosExpedientes:DatoInvalido{
+		public DemasiadosExpedientes(){
+			this.msg = "\nEl abogado ya tiene demasiados expedientes asignados";
+		}
+	}
+
+	public class FaltanExpedientes:DatoInvalido{
+		public FaltanExpedientes() {
+			this.msg = "\nEl abogado ya no tiene mas expedientes asignados";
+		}
+	}
+}
---
+++ TP/Codigo/Estudio/Back-End/Estudio.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using ListaIdNS;
+
+namespace EstudioNS
+{
+	public class Estudio
+	{
+		private ListaExpedientes exps;
+		private ListaAbogados abogados;
+		
+		public Estudio() {
+			this.abogados = new ListaAbogados(this);
+			this.exps = new ListaExpedientes();
+		}
+
+		public ListaAbogados Abogados {
+			get{return this.abogados;}
+		}
+		
+		public ListaExpedientes Expedientes{
+			get{return this.exps;}
+		}
+	}
+	
+	public class ListaExpedientes:ListaId{
+
+        // Excepcion "DemasiadosExpedientes"
+        // No puedo reutilizar el del padre, por el orden de chequeos
+        public void Agregar(Expediente e) {
+        	if ( base.existe(e.Numero) )
+				throw new NumExpedienteRepetido();
+			if (e.Abogado != null) // Se permite asignar despues. Idem al despedir un abogado.
+				e.Abogado.CantExps++; 
+			this.lista.Add(e);
+		}
+
+        //Excepcion "FaltanExpedientes"
+        //Excepcion "DatoInvalido"
+        public override Identificable Eliminar(string numero) {
+			Expediente e = base.posicion(numero); //Excepcion "DatoInvalido"
+			Expediente e = (Expediente) base.Eliminar(numero); //Excepcion "DatoInvalido"
+			if (e.Abogado.CantExps>0)
+            	e.Abogado.CantExps--; 
+			else {
+				this.Agregar(e);
+				throw new FaltanExpedientes();
+			}
+            return e;
+		}
+
+	}
+	
+	public class ListaAbogados:ListaId {
+
+        private Estudio estudio;
+
+        public ListaAbogados(Estudio e) {
+            this.estudio = e;
+        }
+
+        public void Agregar(Abogado a){
+			if ( existe(a.Id) )
+				throw new DniRepetido();
+			this.lista.Add(a);
+		}
+        
+        // Excepcion DatoInvalido()
+		// InconsistenciaExpedientesSinAsignar
+		public override Identificable Eliminar(string dni) {
+			Abogado a = (Abogado) base.Eliminar(dni); //Excepcion DatoInvalido()
+            int j = -1;
+            bool warning = false;
+			ListaExpedientes exps = this.estudio.Expedientes;
+            while ( a.CantExps > 0 && ++j<=exps.Count()-1 ) {
+				if ( ((Expediente)exps.Get(j)).Abogado.Dni == a.Dni ) {
+					((Expediente)exps.Get(j)).Abogado = null;
+					try{
+						a.CantExps--;
+					} catch (FaltanExpedientes err){  
+						warning = true;
+					};
+				}
+			}
+			if (warning)
+				throw new InconsistenciaExpedientesSinAsignar(a);
+            return a;
+        }
+        
+    }
+	
+	public class DniRepetido:DatoInvalido{
+		public DniRepetido(){
+			this.msg = "\nYa existe un abogado con el mismo DNI";
+		}
+	}
+
+	public class NumExpedienteRepetido:DatoInvalido{
+		public NumExpedienteRepetido(){
+			this.msg = "\nHay un expediente registrado con el mismo numero";
+		}
+	}
+
+	
+	public class InconsistenciaExpedientesSinAsignar:DatoInvalido{
+		private Abogado a;
+		public InconsistenciaExpedientesSinAsignar(Abogado a) {
+			this.msg = "\nWARNING: Se detecto que el abogado tenia un conteo erroneo en la cantidad de expedientes asignados";
+			this.a = a;
+		}
+
+		public Abogado abogado{
+			get{return a;}
+		}
+
+}
