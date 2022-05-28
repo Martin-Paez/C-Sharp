667c62b5ccfc3de92af07e93949ed12bf89c15c5
Martin-Paez <mpaez.ok@gmail.com>
2022-05-27 22:16:38 -0300

    1

--- TP/Estudio/Excepciones.cs
+++ TP/Estudio/Excepciones.cs
@@ -1,12 +1,4 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Martin
- * Date: 23/05/2022
- * Time: 19:26
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
 namespace EstudioNS
 {
