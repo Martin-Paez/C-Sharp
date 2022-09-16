b83dfaca2dc76b06cf0f4960cd386f3d4ef0d4b0
Martin-Paez <mpaez.ok@gmail.com>
2022-09-16 13:11:31 -0300

    Primer Commit dentro de Visual Studio Se crearon archivos para cara clase del Factory Method

---
+++ Proyecto 7 - Patrones/TP3/Clases/Fabricas/CmpAlumnos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.TP3.Clases.Estrategias;
+
+namespace TP.TP3.Clases.Fabricas
+{
+    public class ComAlumnos : _CmpAlumnos<Alumno> { }
+    public class _CmpAlumnos<T> : _FabAlumnos<T> where T : Alumno
+    {
+        protected new void SetRand()
+        {
+            if (Criterio is PorLeg)
+                LegRand();
+            else if (Criterio is PorProm)
+                PromRand();
+            else if (Criterio is PorNom)
+                NombreRand();
+            else if (Criterio is PorDni)
+                ((_CmpPersonas<T>)this).SetRand();
+        }
+    }
+}
---
+++ Proyecto 7 - Patrones/TP3/Clases/Fabricas/FabAlumnos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.TP3.Clases.Utiles;
+using TP.TP3.Interfaces.Comparar;
+
+namespace TP.TP3.Clases.Fabricas
+{
+    public class FabAlumnos : _FabAlumnos<Alumno> { }
+
+    public class _FabAlumnos<T> : _CmpPersonas<T> where T : Alumno
+    {
+        protected int? Leg = null;
+        protected float? Prom = null;
+
+        protected void PromRand()
+        {
+            Prom = 10;//GenAleatoriosDeDatos.NumeroAleatorio(11);
+        }
+        protected void LegRand()
+        {
+            Leg = GenAleatoriosDeDatos.NumeroAleatorio(10000);
+        }
+        public new void SetRand()
+        {
+            LegRand();
+            PromRand();
+            ((_FabPersonas<T>)this).SetRand();
+        }
+        protected override T Rand()
+        {
+            SetRand();
+            Alumno a = new(Nombre, Dni, Leg, Prom);
+            if (Criterio != null)
+                a.Cmp = (Comparador<Alumno>)Criterio;
+            return (T)(object)a;
+        }
+    }
+}
